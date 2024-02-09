a620866b4f960d3a346d5ddeabeb3bd1209a046f
smartvnm <laurex@mail.com>
2024-02-09 19:17:07 +0200

    update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from llama_cpp import Llama
 llm = Llama(
-  model_path = "/content/models/7B/",
+  model_path = "/content/models/minicpm-2b-dpo-fp32.Q8_0.gguf",
   n_gpu_layers=-1,
   seed = 1337,
   n_ctx = 2048,
