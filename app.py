9f2fae6daa582cd755061ac926f3bc3c4b29e2e9
smartvnm <laurex@mail.com>
2024-02-09 21:17:11 +0200

    update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from llama_cpp import Llama
 llm = Llama(
   model_path = "/content/models/mistral-7b-instruct-v0.1.Q6_K.gguf",
   n_gpu_layers=-1,
-  n_threads = 2
+  n_threads = 2,
   n_ctx = 2048,
 )
