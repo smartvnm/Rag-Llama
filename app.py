fcd6908a0fcd55a9ed7099acd99a8b183adbe2c2
smartvnm <laurex@mail.com>
2024-02-10 16:12:37 +0200

    update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ print(type(documents))
 for d in documents:
     index.insert(document = d, service_context = service_context)
 query_engine = index.as_query_engine()
-response = query_engine.query("Could you make a blog article from the given context? Return your response which covers the key points of the text and do not miss anything important, please.")
+response = query_engine.query("Could you make a summary from the given context? Return your response which covers the key points of the text and do not miss anything important, please.")
 print(response)
 
 
