665fb9a8977a2c4e8439fbe80008c6017f6175a4
smartvnm <laurex@mail.com>
2024-02-10 10:31:05 +0200

    update DDG.py

---
+++ DDG.py
@@ -0,0 +1,13 @@
+from duckduckgo_search import DDGS
+
+with DDGS() as ddgs:
+    keywords = 'holiday'
+    ddgs_news_gen = ddgs.news(
+      keywords,
+      region="wt-wt",
+      safesearch="off",
+      timelimit="m",
+      max_results=20
+    )
+    for r in ddgs_news_gen:
+        print(r)
