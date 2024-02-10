3f5b194130f5294f0de65de4610adde55586745d
smartvnm <laurex@mail.com>
2024-02-10 18:42:53 +0200

    update DDG.py

--- DDG.py
+++ DDG.py
@@ -6,7 +6,7 @@ if os.path.exists('/content/docs'):
     shutil.rmtree('/content/docs')
 os.mkdir('/content/docs')
 with DDGS() as ddgs:
-    keywords = 'Ukraine'
+    keywords = 'generative ai'
     ddgs_news_gen = ddgs.news(
       keywords,
       region="wt-wt",
